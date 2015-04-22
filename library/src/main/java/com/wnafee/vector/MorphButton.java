28113373aca52629cf6cbefeab0fba1aeed2f0fe
Wael Nafee <wnafee@gmail.com>
2015-04-22 14:39:32 +0200

    add methods to allow MorphButton instantiated from java. Add OnStateChanged listener

--- library/src/main/java/com/wnafee/vector/MorphButton.java
+++ library/src/main/java/com/wnafee/vector/MorphButton.java
@@ -39,6 +39,10 @@ public class MorphButton extends CompoundButton {
         END
     }
 
+    public interface OnStateChangedListener {
+        public void onStateChanged(MorphState changedTo, boolean isAnimating);
+    }
+
     MorphState mState = MorphState.START;
 
     Drawable mStartMorph = null;
@@ -48,6 +52,9 @@ public class MorphButton extends CompoundButton {
     boolean mEndCanMorph = false;
 
     boolean mIsToggling = false;
+    boolean mHasStarted = false;
+
+    private OnStateChangedListener mStateListener;
 
     public MorphButton(Context context) {
         this(context, null);
@@ -77,15 +84,18 @@ public class MorphButton extends CompoundButton {
         if (startResId > 0) {
             mStartMorph = ResourcesCompat.getDrawable(context, startResId);
             mStartCanMorph = isMorphable(mStartMorph);
+//            setDrawableCallback(mStartMorph);
         }
 
         if (endResId > 0) {
             mEndMorph = ResourcesCompat.getDrawable(context, endResId);
             mEndCanMorph = isMorphable(mEndMorph);
+//            setDrawableCallback(mStartMorph);
         }
 
         setState(mState);
         if (autoStart) {
+            mHasStarted = true;
             setState(MorphState.END, true);
         }
     }
@@ -94,12 +104,60 @@ public class MorphButton extends CompoundButton {
         return d != null && d instanceof Animatable;
     }
 
+    private void setDrawableCallback(Drawable d) {
+        if (d != null) {
+            d.setCallback(this);
+        }
+    }
+
+    public void setOnStateChangedListener(OnStateChangedListener l) {
+
+        //Should I invalidate something?
+        if (l != null && l != mStateListener) {
+            mStateListener = l;
+        }
+    }
+
     @Override
     public void toggle() {
+        mHasStarted = true;
         mIsToggling = true;
         setState(mState == MorphState.START ? MorphState.END: MorphState.START, true);
         super.toggle();
         mIsToggling = false;
+    }
+
+    public void setStartDrawable(int rId) {
+        if (rId > 0) {
+            setStartDrawable(ResourcesCompat.getDrawable(getContext(), rId));
+        }
+    }
+
+    public void setStartDrawable(Drawable d) {
+        mStartMorph = d;
+        mStartCanMorph = isMorphable(d);
+//        setDrawableCallback(mStartMorph);
+        setState(mState);
+    }
+
+    public void setEndDrawable(int rId) {
+        if (rId > 0) {
+            setEndDrawable(ResourcesCompat.getDrawable(getContext(), rId));
+        }
+    }
+
+    public void setEndDrawable(Drawable d) {
+        mEndMorph = d;
+        mEndCanMorph = isMorphable(d);
+//        setDrawableCallback(mStartMorph);
+        setState(mState);
+    }
+
+    public void setAutoStart(boolean autoStart) {
+        if (autoStart && !mHasStarted) {
+            mHasStarted = true;
+            setState(MorphState.END, true);
+        }
     }
 
     private boolean beginStartAnimation() {
@@ -155,6 +213,10 @@ public class MorphButton extends CompoundButton {
      */
     @SuppressWarnings("deprecation")
     public void setState(MorphState state, boolean animate) {
+        if (mState == state && mHasStarted) {
+            return;
+        }
+
         if (state == MorphState.START) {
             setBackgroundDrawable(mEndCanMorph ? mEndMorph : mStartMorph);
             beginEndAnimation();
@@ -170,6 +232,10 @@ public class MorphButton extends CompoundButton {
         }
 
         mState = state;
+
+        if (mStateListener != null) {
+            mStateListener.onStateChanged(state, animate);
+        }
     }
 
     @Override
